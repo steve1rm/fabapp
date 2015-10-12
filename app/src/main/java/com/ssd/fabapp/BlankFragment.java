172c2e248640b7a099f25b7f0d1ff892a9ec42bc
Steve <steve1.rm@gmail.com>
2015-10-12 08:15:42 +0700

    Updated comments, styles, and colors

--- app/src/main/java/com/ssd/fabapp/BlankFragment.java
+++ app/src/main/java/com/ssd/fabapp/BlankFragment.java
@@ -13,18 +13,27 @@ import android.widget.Toast;
  */
 public class BlankFragment extends Fragment {
 
+    /* Some data that we want to pass into the fragment example only */
     private int mData;
 
     public BlankFragment() {
         // Required empty public constructor
     }
 
+    /* key that maps to our data when setting and retrieving */
     private static final String DATA_KEY = "data_key";
+
+    /* Factory method for returning a new instance of our fragment,
+       sets the data into a bundle and sets it as an argument for the fragment.
+       NEVER use a overloaded constructor for doing this, as an overloaded constructor
+       will not be called when the fragment has to re-create itself.
+       This is the ONLY way you can pass data safely to fragments */
     public static BlankFragment getNewInstance(int data) {
-        Bundle args = new Bundle();
+        final Bundle args = new Bundle();
         args.putInt(DATA_KEY, data);
 
         BlankFragment blankFragment = new BlankFragment();
+        /* Sets the argument to be retrieved in the CreateView later */
         blankFragment.setArguments(args);
 
         return blankFragment;
@@ -32,15 +41,18 @@ public class BlankFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        final View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        FloatingActionButton fabButton = (FloatingActionButton)view.findViewById(R.id.fabButton);
+        final FloatingActionButton fabButton = (FloatingActionButton)view.findViewById(R.id.fabButton);
 
+        /* Here we are getting the contents from the bundle after doing some sanity checks */
         Bundle bundle = getArguments();
         if(bundle != null) {
+            /* If there is no value, set the default 0 value for the data */
             mData = bundle.getInt(DATA_KEY, 0);
         }
 
+        /* The FAB will display the contents of our data in a toast message */
         fabButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +62,4 @@ public class BlankFragment extends Fragment {
 
         return view;
     }
-
-
 }
--- app/src/main/java/com/ssd/fabapp/MainActivity.java
+++ app/src/main/java/com/ssd/fabapp/MainActivity.java
@@ -12,10 +12,17 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        /* This is where we will add our fragment */
+        /* This is where we will add our fragment
+           If savedInstanceState is null then this the first time MainActivity has been created
+           so we add the fragment.
+           And time after that savedInstanceState won't be null so no need to add the fragment again */
         if(savedInstanceState == null) {
             FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            /* First parameter is the id of the layout we want to add this fragment into
+               Second parameter we get a new instance of the fragment has pass in some data
+               Third parameter is good practice to add this, the string tag and for referencing fragments (headless fragments) */
             fragmentTransaction.add(R.id.fragment_container, BlankFragment.getNewInstance(1234), "blankFragment");
+            /* final step commit the fragment thus displaying it in our UI */
             fragmentTransaction.commit();
         }
     }
